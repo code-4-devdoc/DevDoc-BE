6101c342c943ef3a3c3772e468afcc482f02f28c
dhkswls1998 <dhkswls1998@naver.com>
2024-06-18 09:39:05 +0900

    init

--- src/main/java/com/devdoc/backend/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.devdoc.backend.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
